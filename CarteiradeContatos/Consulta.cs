345f9b6d8910fc84e486317376e571c08a83cb15
Victor Miranda <VictorBelvizoMiranda@hotmail.com>
2025-01-02 15:46:36 -0300

    versão rodando, faltando campos para pesquisa e adição de contato, com a persistencia de dados, e testes

--- CarteiradeContatos/Consulta.cs
+++ CarteiradeContatos/Consulta.cs
@@ -2,19 +2,21 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-public class Consulta : PageModel
-{
-    public IList<Contato> Contatos { get; set; }
-
-    private readonly MyDbContext _context;
-
-    public Consulta(MyDbContext context)
+namespace CarteiradeContatos { 
+    public class Consulta : PageModel
     {
-        _context = context;
-    }
+        public IList<Contato> Contatos { get; set; }
 
-    public async Task OnGetAsync()
-    {
-        Contatos = await _context.Contatos.ToListAsync();
+        private readonly MyDbContext _context;
+
+        public Consulta(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Contatos = await _context.Contatos.ToListAsync();
+        }
     }
 }
