a7b92cb1a7f753a9f58dc3d05dfd905b05332e03
Victor Miranda <VictorBelvizoMiranda@hotmail.com>
2025-01-25 18:48:27 -0300

    ajuste handler

---
+++ CarteiradeContatos/CustomCircuitHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+using System.Collections.Concurrent;
+
+public class CustomCircuitHandler : CircuitHandler
+{
+    private readonly ConcurrentDictionary<string, Circuit> _circuits = new ConcurrentDictionary<string, Circuit>();
+
+    public override Task OnCircuitOpenedAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        _circuits[circuit.Id] = circuit;
+        return Task.CompletedTask;
+    }
+
+    public override Task OnCircuitClosedAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        _circuits.TryRemove(circuit.Id, out _);
+        return Task.CompletedTask;
+    }
+
+    public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // Lógica adicional quando a conexão é estabelecida
+        return Task.CompletedTask;
+    }
+
+    public override Task OnConnectionDownAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // Lógica adicional quando a conexão é perdida
+        return Task.CompletedTask;
+    }
+
+    public IEnumerable<Circuit> GetActiveCircuits() => _circuits.Values;
+}
--- CarteiradeContatos/Program.cs
+++ CarteiradeContatos/Program.cs
@@ -4,10 +4,11 @@ builder.Services.AddServerSideBlazor(options =>
 {
     options.DetailedErrors = true;
 });
+builder.Services.AddScoped<CustomCircuitHandler>();
 builder.Services.AddSingleton<ContatoService>();
 builder.Services.AddSignalR();
 builder.Services.AddAntiforgery();
-
+builder.Services.AddScoped<Microsoft.AspNetCore.Components.Server.Circuits.CircuitRegistry>();
 
 var app = builder.Build();
 
