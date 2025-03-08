56e8b4c201cfb037f8e348c6d79f4870f3abe641
Victor Miranda <VictorBelvizoMiranda@hotmail.com>
2025-03-08 08:23:13 -0300

    ultimas alterações

--- CarteiradeContatos/ContatoService.cs
+++ CarteiradeContatos/ContatoService.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public class ContatoService
+﻿public class ContatoService
 {
     private readonly List<Contato> _contatos = new List<Contato>();
+
+    public Task<List<Contato>> GetContatosAsync()
+    {
+        return Task.FromResult(_contatos);
+    }
 
     public Task AddContatoAsync(Contato contato)
     {
         _contatos.Add(contato);
         return Task.CompletedTask;
     }
-
-    public Task<List<Contato>> GetContatosAsync()
-    {
-        return Task.FromResult(_contatos);
-    }
 }
+
